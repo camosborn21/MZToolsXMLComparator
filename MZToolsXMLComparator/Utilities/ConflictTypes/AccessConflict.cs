f225063238bafa44b2154f880aa9ba803fef9045
Cameron Osborn <camosborn21@msn.com>
2018-03-27 15:05:27 -0700

    20180327_1505

--- MZToolsXMLComparator/Utilities/ConflictTypes/AccessConflict.cs
+++ MZToolsXMLComparator/Utilities/ConflictTypes/AccessConflict.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Media;
 using MZToolsXMLComparator.Models;
 
@@ -27,47 +28,84 @@ namespace MZToolsXMLComparator.Utilities.ConflictTypes
 
 		public void Resolve()
 		{
-			bool finished = false;
-			while (finished == false)
+			//bool finished = false;
+			//while (finished == false)
+			//{
+			_c.Lines(2);
+			_c.HorizontalRule();
+			Console.WriteLine(@"** RESOLVE ACCESS CONFLICT (press a key to select an option) **");
+			Console.WriteLine(Description);
+			_c.Line();
+			int count = 1;
+			int selectedTemplate = -1;
+			foreach (CodeTemplate template in ConflictedTemplates)
 			{
-				_c.Lines(2);
-				_c.HorizontalRule();
-				Console.WriteLine(@"** RESOLVE ACCESS CONFLICT (press a key to select an option) **");
-				Console.WriteLine(Description);
-				_c.Line();
-				Console.WriteLine(@"A. [ALL]" + _c.GetAlignmentSpacing(3,20) + @"Display all conflict templates current access settings");
-				Console.WriteLine(@"S. [SELECT]" + _c.GetAlignmentSpacing(6,20) + @"Select which template will resolve this conflict");
-				char keyPressed;
-				Console.Write(@"Your choice is: ");
-				keyPressed = Console.ReadKey().KeyChar;
-				_c.Line();
-				switch (keyPressed)
-				{
-					case 'A':
-					case 'a':
-						DisplayAccessConflicts();
-						break;
-					case 'S':
-					case 's':
-						SelectResolution();
-						break;
-					default:
-						Console.WriteLine(@"Invalid selection.");
-						break;
-				}
-				finished = true;
+				Console.WriteLine(count + @". " + template.Description + @" from " + template.ParentGuid + @"-" + template.Id);
+				Console.WriteLine(@"	Expansion Keyword : " + template.ExpansionKeyword);
+				Console.WriteLine(@"	Command Name      : " + template.CommandName);
+
+				count++;
 			}
+			Console.Write(@"Your choice is: ");
+			selectedTemplate = Int32.Parse(Console.ReadKey().KeyChar.ToString());
+			while (selectedTemplate - 1 < 0 || selectedTemplate - 1 >= ConflictedTemplates.Count)
+			{
+				Console.WriteLine(@"Invalid Selection. Choose a template from the list above.");
+				selectedTemplate = Int32.Parse(Console.ReadKey().KeyChar.ToString());
+			}
+			ResolutionTemplate = ConflictedTemplates.ElementAt(selectedTemplate - 1);
+
+
+			//Console.WriteLine(@"A. [ALL]" + _c.GetAlignmentSpacing(3,20) + @"Display all conflict templates current access settings");
+			//Console.WriteLine(@"S. [SELECT]" + _c.GetAlignmentSpacing(6,20) + @"Select which template will resolve this conflict");
+			//char keyPressed;
+			//Console.Write(@"Your choice is: ");
+			//keyPressed = Console.ReadKey().KeyChar;
+			//_c.Line();
+			//switch (keyPressed)
+			//{
+			//	case 'A':
+			//	case 'a':
+			//		DisplayAccessConflicts();
+			//		break;
+			//	case 'S':
+			//	case 's':
+			//		SelectResolution();
+			//		break;
+			//	default:
+			//		Console.WriteLine(@"Invalid selection.");
+			//		break;
+			//}
+			//	finished = true;
+			//}
 		}
 
-		private void SelectResolution()
-		{
-			throw new NotImplementedException();
-		}
+		//private void SelectResolution()
+		//{
+		//	int count = 1;
+		//	foreach (CodeTemplate template in ConflictedTemplates)
+		//	{
+		//		Console.WriteLine(count + @". " + template.Description + @" from " + template.ParentGuid + @"-" + template.Id);
+		//		Console.WriteLine(@"	Expansion Keyword : " + template.ExpansionKeyword);
+		//		Console.WriteLine(@"	Command Name      : " + template.CommandName);
 
-		private void DisplayAccessConflicts()
-		{
-			throw new NotImplementedException();
-		}
+		//		count++;
+		//	}
+		//}
+
+		//private void DisplayAccessConflicts()
+		//{
+		//	Console.WriteLine(@"Conflict Template Information");
+		//	int count = 1;
+		//	foreach (CodeTemplate template in ConflictedTemplates)
+		//	{
+		//		Console.WriteLine(count + @". " + template.Description + @" from " + template.ParentGuid + @"-" + template.Id);
+		//		Console.WriteLine(@"	Expansion Keyword : " + template.ExpansionKeyword);
+		//		Console.WriteLine(@"	Command Name      : " + template.CommandName);
+
+		//		count++;
+		//	}
+		//}
 
 
 	}
